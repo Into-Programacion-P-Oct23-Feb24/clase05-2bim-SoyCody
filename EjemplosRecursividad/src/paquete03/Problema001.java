1c9e1a0f8254ac2378f1b17685d0fb706e9c4906
Cody <codycabreraa@gmail.com>
2024-01-11 15:53:45 -0500

    Cambios

---
+++ EjemplosRecursividad/src/paquete03/Problema001.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author Cody
+ */
+public class Problema001 {
+    public static void main(String[] args){
+        
+    }
+    public static String [][] obtenerCadena(){
+        String arreglo [][] = {{"Yantzaza", "Los Encuentros", "El Pangui"},
+                                    {"Chicaña", "San Vicente", "Panguitza"},
+                                    {"Macará", "Lago Agrio", "Catamayo"}};
+        
+        return arreglo;
+    }
+
+}
